bef5411e565618a2dd8e645eed5adbd35b42d798
Bullets-jyr <jyr@bullets.co.kr>
2023-07-19 15:11:52 +0900

    04. 카메라 모듈 구성

---
+++ face_recognition/src/main/java/kr/co/bullets/face_recognition/Camera.kt
@@ -0,0 +1,61 @@
+package kr.co.bullets.face_recognition
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executors
+
+class Camera(private val context: Context) {
+    private val preview by lazy {
+        Preview.Builder()
+            .build()
+            .also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+    }
+
+    private val cameraSelector by lazy {
+        CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+            .build()
+    }
+
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var previewView: PreviewView
+
+    private var cameraExecutor = Executors.newSingleThreadExecutor()
+
+    fun initCamera(layout: ViewGroup) {
+        previewView = PreviewView(context)
+        layout.addView(previewView)
+    }
+
+    private fun openPreview() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+            .also { providerFuture ->
+                providerFuture.addListener({
+
+                }, ContextCompat.getMainExecutor(context))
+            }
+    }
+
+    private fun startPreview(context: Context) {
+        val cameraProvider = cameraProviderFuture.get()
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                context as LifecycleOwner,
+                cameraSelector,
+                preview
+            )
+        } catch (e: Exception) {
+            e.stackTrace
+        }
+    }
+}
