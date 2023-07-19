868ed1eaed8874ee16ed94d3b3395dab641d9498
Bullets-jyr <jyr@bullets.co.kr>
2023-07-19 14:47:04 +0900

    03. Main UI 그리기

--- app/src/main/java/kr/co/bullets/part3chapter1/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part3chapter1/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.bullets.part3chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kr.co.bullets.part3chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            startDetectButton.setOnClickListener {
+                it.isVisible = false
+            }
+        }
     }
 }
